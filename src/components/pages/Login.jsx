0e715fc336fa746403db7eb1007dc21bd56b2bfc
abhaythanak <abhaythanak@gmail.com>
2025-07-25 23:36:15 +0530

    useState hook use for get data from input field

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,14 +1,41 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center m-14">
       <div className="card  card-dash bg-base-300 w-96">
         <div className="card-body">
-          <h2 className="card-title">Login</h2>
+          <h2 className="card-title justify-center">Login</h2>
           <div className="">
-            <label></label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label my-3">
+                <span className="label-text">Email Id</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered max-w-xs"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full  max-w-xs ">
+              <div className="label my-3">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="password"
+                placeholder="Type here"
+                className="input input-bordered max-w-xs"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
           </div>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary">Login</button>
           </div>
         </div>
       </div>
